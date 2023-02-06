0ccd069049e4a97feffa026eac06ad1d8fc0829b
Scarlat Pereira <engcivil.scarlat@gmail.com>
2023-02-06 17:13:43 -0300

    feat: add requirement 3

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const { getAllManager, getManagerById } = require('./talkerManager');
+const generateToken = require('./utils/generateToken');
 
 const app = express();
 app.use(express.json());
@@ -27,5 +28,10 @@ app.get('/talker/:id', async (req, res) => {
   if (!talker) {
     return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
   }
-  return res.status(200).json(talker);
+  return res.status(HTTP_OK_STATUS).json(talker);
+});
+
+app.post('/login', (req, res) => {
+  const token = generateToken();
+    return res.status(HTTP_OK_STATUS).json({ token });
 });
